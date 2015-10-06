5c5d8000b5561a45998f21fe27c854778c75df56
Raphael Weinfurter <raphaelweinfurter@live.at>
2015-10-06 18:38:48 +0200

    finally

---
+++ app/src/main/java/at/robo4you/missioncontrol/ViewPagerAdapter.java
@@ -0,0 +1,53 @@
+package at.robo4you.missioncontrol;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Raphael on 04.10.2015.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+    // Build a Constructor and assign the passed Values to appropriate values in the class
+    public ViewPagerAdapter(FragmentManager fm,CharSequence mTitles[], int mNumbOfTabsumb) {
+        super(fm);
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+    }
+
+    //This method return the fragment for the every position in the View Pager
+    @Override
+    public Fragment getItem(int position) {
+
+        if(position == 0) // if the position is 0 we are returning the First tab
+        {
+            SensorDisplay sensorTab = new SensorDisplay();
+            sensorTab.sensor_text = "Test Sensor";
+            return sensorTab;
+        }
+        else             // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
+        {
+            ControlDisplay controlTab = new ControlDisplay();
+            controlTab.motor_name = "Test Motor";
+            return controlTab;
+        }
+    }
+
+    // This method return the titles for the Tabs in the Tab Strip
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    // This method return the Number of tabs for the tabs Strip
+
+    @Override
+    public int getCount() {
+        return NumbOfTabs;
+    }
+}
