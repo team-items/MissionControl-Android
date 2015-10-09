e3bbe17c34261698046d76214d7de552d5325113
Florian <florianungersbock@gmail.com>
2015-10-09 16:22:20 +0200

    added emty login activity
    added emty login activity

---
+++ app/src/main/java/robo4you/at/missioncontrolandroid/LoginScreen.java
@@ -0,0 +1,14 @@
+package robo4you.at.missioncontrolandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class LoginScreen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+
+}
