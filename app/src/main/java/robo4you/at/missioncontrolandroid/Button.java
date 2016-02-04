9ac04217d74f7d7c4ab6c789a170ebbf4267747f
Florian <florianungersbock@gmail.com>
2016-02-04 22:46:21 +0100

    redid layout of buttons

--- app/src/main/java/robo4you/at/missioncontrolandroid/Button.java
+++ app/src/main/java/robo4you/at/missioncontrolandroid/Button.java
@@ -1,6 +1,7 @@
 package robo4you.at.missioncontrolandroid;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,8 +18,8 @@ public class Button extends Controller {
     private Context context;
     private LinearLayout layout;
     private String label;
-    private Switch switchWidget;
     private TextView label_tv;
+    private android.widget.Button button;
 
     public Button(String label, int value, final Context context) {
         this.value = value;
@@ -31,7 +32,28 @@ public class Button extends Controller {
         this.label_tv = (TextView) layout.findViewById(R.id.motor_label);
         this.label_tv.setTypeface(MainActivity.getTypeface());
         label_tv.setText(label);
-        this.switchWidget = (Switch) layout.findViewById(R.id.switchWidget);
+
+        this.button = (android.widget.Button)layout.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            private boolean status = true;
+            @Override
+            public void onClick(View v) {
+                if(status){
+                    button.setBackgroundColor(-1);
+                    button.setTextColor(context.getResources().getColor(R.color.itemsRed));
+                    button.setText("0");
+                    value = 0;
+                }else{
+                    button.setBackgroundColor(context.getResources().getColor(R.color.itemsRed));
+                    button.setTextColor(-1);
+                    button.setText("1");
+                    value = 1;
+                }
+                status = !status;
+            }
+        });
+
         return layout;
     }
 
@@ -52,4 +74,5 @@ public class Button extends Controller {
     public boolean isClicked() {
         return value == 1;
     }
+
 }
